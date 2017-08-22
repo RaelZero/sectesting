7ad13cca99de339d754f05799459337d6187c645
RaelZero <gaz.gong@gmail.com>
2017-08-22 12:26:47 -0400

    Graph update automation

--- dot2jpg.py
+++ dot2jpg.py
@@ -3,14 +3,14 @@
 from subprocess import call
 from os import listdir
 
-basedir = 'graphs/'
+basedir = 'pixy/graphs/'
 
 files = listdir(basedir)
 names = []
 
 for f in files:
     names.append(f[:-4])
-    
+
 for n in names:
     out = open('jpegs/' + n + '.jpg', 'w')
     call(['dot', '-Tjpg', basedir + n + '.dot'], stdout=out)
