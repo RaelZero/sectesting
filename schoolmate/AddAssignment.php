4a5d4382ef5c328ec897a46a045987599ea490a8
RaelZero <gaz.gong@gmail.com>
2017-08-22 11:35:08 -0400

    Mistake on #11

--- schoolmate/AddAssignment.php
+++ schoolmate/AddAssignment.php
@@ -32,7 +32,7 @@
   <input type='hidden' name='addassignment' value=''>
   <input type='hidden' name='page2' value='$page2'>
   <input type='hidden' name='logout'>
-  <input type='hidden' name='selectclass' value='.intval($_POST[selectclass]).' />
+  <input type='hidden' name='selectclass' value='". intval($_POST[selectclass]) ."' />
   <input type='hidden' name='page' value='$page'>
 
  </form>
