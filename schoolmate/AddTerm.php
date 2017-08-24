56baaf9292da468796c6e1f28ed0fe01745abea8
RaelZero <gaz.gong@gmail.com>
2017-08-24 05:16:15 -0400

    Fix #19

--- schoolmate/AddTerm.php
+++ schoolmate/AddTerm.php
@@ -26,9 +26,9 @@
    </table>
 
   <input type='hidden' name='addterm' value=''>
-  <input type='hidden' name='page2' value='$page2'>
+  <input type='hidden' name='page2' value='".intval($page2)."'>
   <input type='hidden' name='logout'>
-  <input type='hidden' name='page' value='$page'>
+  <input type='hidden' name='page' value='".intval($page)."'>
 
  </form>
 
