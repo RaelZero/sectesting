36ecd41a08426591df73ff4a397c9e35342de0d3
RaelZero <gaz.gong@gmail.com>
2017-08-24 12:22:03 +0200

    Fix #63, #70, #71

--- schoolmate/AddSemester.php
+++ schoolmate/AddSemester.php
@@ -48,9 +48,9 @@
    </table>
 
   <input type='hidden' name='addsemester' value=''>
-  <input type='hidden' name='page2' value='$page2'>
+  <input type='hidden' name='page2' value='".intval($page2)."'>
   <input type='hidden' name='logout'>
-  <input type='hidden' name='page' value='$page'>
+  <input type='hidden' name='page' value='".intval($page)."'>
 
  </form>
 
--- schoolmate/AddStudent.php
+++ schoolmate/AddStudent.php
@@ -39,9 +39,9 @@ print("	 </select>
    </table>
 
   <input type='hidden' name='addstudent' value=''>
-  <input type='hidden' name='page2' value='$page2'>
+  <input type='hidden' name='page2' value='".intval($page2)."'>
   <input type='hidden' name='logout'>
-  <input type='hidden' name='page' value='$page'>
+  <input type='hidden' name='page' value='".intval($page)."'>
 
  </form>
 
