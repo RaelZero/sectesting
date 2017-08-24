1fd5188051e0568ad4f3b12c008415c4fb36548e
RaelZero <gaz.gong@gmail.com>
2017-08-24 12:05:25 +0200

    Fix #11

--- schoolmate/AddAssignment.php
+++ schoolmate/AddAssignment.php
@@ -30,10 +30,10 @@
    </table>
 
   <input type='hidden' name='addassignment' value=''>
-  <input type='hidden' name='page2' value='$page2'>
+  <input type='hidden' name='page2' value='".intval($page2)."'>
   <input type='hidden' name='logout'>
   <input type='hidden' name='selectclass' value='". intval($_POST[selectclass]) ."' />
-  <input type='hidden' name='page' value='$page'>
+  <input type='hidden' name='page' value='".intval($page)."'>
 
  </form>
 
