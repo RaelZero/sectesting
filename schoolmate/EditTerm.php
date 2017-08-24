78706d197487ead91d260337298d2350ce3d6489
RaelZero <gaz.gong@gmail.com>
2017-08-24 05:34:12 -0400

    Fix #44

--- schoolmate/EditTerm.php
+++ schoolmate/EditTerm.php
@@ -36,10 +36,10 @@
    </table>
 
   <input type='hidden' name='editterm'>
-  <input type='hidden' name='termid' value='$id[0]'>
-  <input type='hidden' name='page2' value='$page2'>
+  <input type='hidden' name='termid' value='".intval($id[0])."'>
+  <input type='hidden' name='page2' value='".intval($page2)."'>
   <input type='hidden' name='logout'>
-  <input type='hidden' name='page' value='$page'>
+  <input type='hidden' name='page' value='".intval($page)."'>
 
  </form>
 
